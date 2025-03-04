27fe1e23666ae3782a29b244db0617ea151c72de
Ruvik <Kerinaguilera@gmail.com>
2025-03-04 16:54:43 +0100

    Ahora mejoramos el programa piendo tu nombre por texto para luego mostrar un saludo

--- Saludar.java
+++ Saludar.java
@@ -1,7 +1,33 @@
+import java.util.Scanner;
+
 public class Saludar {
     public static void main(String[] args) {
+        String nombre = "";
+        String apellidos = "";
+        Scanner scanner = new Scanner(System.in);
+
+        // Encabezado
         System.out.println("*****************************");
-        System.out.println("*        Hola Mundo!        *");
+        System.out.println("*        Hola a todos!      *");
         System.out.println("*****************************");
+        System.out.println();
+
+        // Solicitar nombre y apellidos
+        System.out.print("Introduce tu nombre: ");
+        nombre = scanner.nextLine();
+        System.out.print("Introduce tu apellido: ");
+        apellidos = scanner.nextLine();
+
+        System.out.println();
+        System.out.println("*****************************");
+        System.out.println("*  Hola " + nombre + "!       *");
+        System.out.println("*  Gracias por usar nuestro   *");
+        System.out.println("*  programa.                  *");
+        System.out.println("*  Esperamos que lo disfrutes! *");
+        System.out.println("*  Guardaremos tus datos:     *");
+        System.out.println("*  " + nombre + " " + apellidos + "  *");
+        System.out.println("*****************************");
+        System.out.println("Saludos,");
+        System.out.println("rt: Gladus");
     }
 }
