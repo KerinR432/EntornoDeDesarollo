b188136cb9dafeba7c73eecc7947d17bfd7ce873
KerinR432 <kerinaguilera@gmail.com>
2025-05-08 12:34:15 +0200

    Testeo

---
+++ SubcripcionPrueba.java
@@ -0,0 +1,8 @@
+public class SubcripcionPrueba {
+    public static void main(String[] args) {
+        Subscripcion n1 = new Subscripcion(30,10);
+        System.out.println(n1.precioPorMes());
+        n1.cancel();
+        System.out.println(n1.precioPorMes());
+    }
+}
---
+++ SubscripcionTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SubscripcionTest {
+    @Test
+    public void testPrecioPorMes() {
+        double espera = 1;
+        Subscripcion s = new Subscripcion(200, 2);
+        double resultado = s.precioPorMes();
+        assertEquals(espera, resultado, 0);
+    }
+
+    @Test
+    public void testPrecioPorMes2() {
+        double espera = 67;
+        Subscripcion s = new Subscripcion(200, 3);
+        double resultado = s.precioPorMes();
+        assertEquals(espera, resultado, 0);
+    }
+}
