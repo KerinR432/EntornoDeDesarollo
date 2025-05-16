f4fbc9ed99afdca3ac7c852346744eb0958f87f5
KerinR432 <kerinaguilera@gmail.com>
2025-05-16 08:49:33 +0200

    Pruebas

--- src/OperadorAritmetico.java
+++ src/OperadorAritmetico.java
@@ -1,6 +1,19 @@
 package src;
 
 public class OperadorAritmetico {
+   /**
+    * Esto es un codigo para sacar operaciones Aritmeticas utilizando 2 funciones.
+    * @code suma.
+    * @code division.
+    * donde devuelve un resultado o una exception.
+    * @param a un parametro que utilizaremos para realizar las operaciones.
+    * @param b otro parametro que utilizamos para realizas operaciones.
+    * @param suma este metodo iguala dos numeros que pasemos por parametro.
+    * @return devuelve un resultado.
+    * @param division este metodo como el anterior divide dos numeros que pasemos por parametro.
+    * @return devuelve un resultado.
+    * @author pepe.
+    * */
     public static int suma(int a, int b) {
         return a + b;
     }
